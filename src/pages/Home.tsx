7e29923fcd0428fe123c26558d1446acfa599f95
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-02-18 19:15:37 +0100

    Implement note fetching, creation, and loading state in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,98 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
-// const apiUrl = import.meta.env.VITE_API_URL;
+import { useNotes } from "../hooks/useNotes";
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export const Home = () => {
-  const [notes] = useState<any[]>([
-    {
-      id: 1,
-      title: "Note 1",
-      content: "Content 1",
-    },
-    {
-      id: 2,
-      title: "Note 2",
-      content: "Content 2",
-    },
-  ]);
-  // useEffect(() => {
-  //   const fetchNotes = async () => {
-  //     try {
-  //       const response = await fetch(`${apiUrl}/notes`);
-  //       const data = await response.json();
-  //       setNotes(data);
-  //     } catch (error) {
-  //       console.error("Error loading notes:", error);
-  //     }
-  //   };
-  //   fetchNotes();
-  // }, []);
+  const { notes, updateAllNotes } = useNotes();
+  const [isLoading, setIsLoading] = useState(notes.length === 0);
+  const [isCreatingNote, setIsCreatingNote] = useState(false);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/notes`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch notes");
+        }
+        const data = await response.json();
+        updateAllNotes(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, [updateAllNotes]);
+
+  const handleCreateNote = async () => {
+    const newNoteBody = prompt("Enter note body:");
+    if (!newNoteBody) {
+      return;
+    }
+    try {
+      setIsCreatingNote(true);
+      const newNote = {
+        body: newNoteBody,
+      };
+      const response = await fetch(`${apiUrl}/notes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newNote),
+      });
+      const data = await response.json();
+      updateAllNotes([...notes, data]);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsCreatingNote(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p className="text-2xl">Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-8">
       <p className="text-3xl font-bold text-center">My notes</p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <div className="text-gray-400 bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow text-center">
+        <button
+          disabled={isCreatingNote}
+          type="button"
+          onClick={handleCreateNote}
+          className="text-gray-400 bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow text-center disabled:opacity-50 disabled:cursor-not-allowed"
+        >
           <h2 className="text-3xl">+</h2>
           <p>Create a new note</p>
-        </div>
-        {notes.map(({ id, title, content }) => (
-          <Link key={id} to={`/notes/${id}`}>
+        </button>
+        {notes.map(({ id, body }) => (
+          <Link key={id} to={`/note/${id}`}>
             <div className="bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow">
-              <h2 className="text-xl font-semibold mb-2">{title}</h2>
-              <p className="text-gray-600 truncate">{content}</p>
+              <h2 className="text-xl font-semibold mb-2">Note title</h2>
+              <p className="text-gray-600 truncate">{body}</p>
             </div>
           </Link>
         ))}
       </div>
+      {notes.length === 0 && (
+        <div>
+          <p className="text-center text-gray-400">No notes found</p>
+          <p className="text-center text-gray-400">
+            Create a new note to get started
+          </p>
+        </div>
+      )}
     </div>
   );
 };

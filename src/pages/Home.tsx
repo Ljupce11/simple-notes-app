d6c889a27c398c78fd4fe122ee970a80b029b7a0
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-02-18 20:26:12 +0100

    Refactor Home component to use form for note creation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,17 +3,21 @@ import { Link } from "react-router";
 
 import { useNotes } from "../hooks/useNotes";
 
-const apiUrl = import.meta.env.VITE_API_URL;
-
+// const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrlWithSession = import.meta.env.VITE_API_URL_WITH_SESSION;
 export const Home = () => {
   const { notes, updateAllNotes } = useNotes();
   const [isLoading, setIsLoading] = useState(notes.length === 0);
   const [isCreatingNote, setIsCreatingNote] = useState(false);
+  const [newNoteBody, setNewNoteBody] = useState("");
 
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const response = await fetch(`${apiUrl}/notes`);
+        const response = await fetch(`${apiUrlWithSession}/notes`);
+        // const response2 = await fetch(`${apiUrl}/users`);
+        // console.log(response2);
+
         if (!response.ok) {
           throw new Error("Failed to fetch notes");
         }
@@ -29,17 +33,14 @@ export const Home = () => {
     fetchNotes();
   }, [updateAllNotes]);
 
-  const handleCreateNote = async () => {
-    const newNoteBody = prompt("Enter note body:");
-    if (!newNoteBody) {
-      return;
-    }
+  const handleCreateNote = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     try {
       setIsCreatingNote(true);
       const newNote = {
         body: newNoteBody,
       };
-      const response = await fetch(`${apiUrl}/notes`, {
+      const response = await fetch(`${apiUrlWithSession}/notes`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -48,6 +49,7 @@ export const Home = () => {
       });
       const data = await response.json();
       updateAllNotes([...notes, data]);
+      setNewNoteBody("");
     } catch (error) {
       console.error(error);
     } finally {
@@ -66,21 +68,36 @@ export const Home = () => {
   return (
     <div className="flex flex-col gap-8">
       <p className="text-3xl font-bold text-center">My notes</p>
+      <div className="flex justify-center">
+        <div className="w-xl bg-white shadow-md rounded-lg p-4">
+          <form className="flex flex-col gap-4" onSubmit={handleCreateNote}>
+            <textarea
+              id="new-note-body"
+              name="new-note-body"
+              aria-label="New note body"
+              placeholder="Enter your note here..."
+              className="border border-gray-300 rounded-lg w-full h-32 p-2 disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={isCreatingNote}
+              value={newNoteBody}
+              required
+              onChange={(e) => setNewNoteBody(e.target.value)}
+            />
+            <button
+              type="submit"
+              className="bg-black text-white px-4 py-2 rounded-lg cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={isCreatingNote}
+            >
+              {isCreatingNote ? "Adding note..." : "Add note"}
+            </button>
+          </form>
+        </div>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <button
-          disabled={isCreatingNote}
-          type="button"
-          onClick={handleCreateNote}
-          className="text-gray-400 bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow text-center disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          <h2 className="text-3xl">+</h2>
-          <p>Create a new note</p>
-        </button>
         {notes.map(({ id, body }) => (
           <Link key={id} to={`/note/${id}`}>
             <div className="bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow">
-              <h2 className="text-xl font-semibold mb-2">Note title</h2>
-              <p className="text-gray-600 truncate">{body}</p>
+              <h2 className="text-lg font-semibold mb-2">Note title</h2>
+              <p className="text-gray-500 truncate">{body}</p>
             </div>
           </Link>
         ))}

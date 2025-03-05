28e52f9f4e87335ee1d5c3f693c0d7d402bd63e8
Ljubomir Bojadziev <ljubomir.bojadziev@gmail.com>
2025-03-05 14:36:50 +0100

    Add full height to NoteCard component

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const NoteCard = memo(({ id, body }: Props) => (
   <Link to={`/note/${id}`}>
-    <div className="bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow">
+    <div className="bg-white shadow-md rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow h-full">
       <h2 className="text-lg font-semibold mb-2">Note title</h2>
       <p className="text-gray-500 truncate">{body}</p>
     </div>
